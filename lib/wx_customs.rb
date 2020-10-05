50d3156a9a91f5ceab94ec9575d10accb5840e14
zhaqiang <hugochougt@gmail.com>
2020-10-05 21:26:09 +0800

    feat: parameters signature generation

--- lib/wx_customs.rb
+++ lib/wx_customs.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "wx_customs/version"
+require "wx_customs/sign"
 
 module WxCustoms
   class Error < StandardError; end
---
+++ test/wx_customs/sign_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module WxCustoms
+  class SignTest < Minitest::Test
+    def setup
+      @params = {
+        appid: "wxd930ea5d5a258f4f",
+        mch_id: "10000100",
+        device_info: "1000",
+        body: "test",
+        nonce_str: "ibuaiVcKdpRxkhJA"
+      }
+      @key = "192006250b4c09247ec02edce69f6a2d"
+      @sign = "9A0A8659F005D6984697E2CA0A9CF3B7"
+    end
+
+    def test_generate_valid_sign
+      assert_equal @sign, WxCustoms::Sign.generate(@params, @key)
+    end
+
+    def test_generate_valid_md5_sign
+      assert_equal @sign, WxCustoms::Sign.generate(@params, @key, WxCustoms::Sign::SIGN_ALGO_MD5)
+    end
+
+    def test_raise_argument_error_when_passing_in_non_supported_sign_algorithm
+      assert_raises ArgumentError do
+        WxCustoms::Sign.generate(@params, @key, "NON_SUPPORTED")
+      end
+    end
+  end
+end
